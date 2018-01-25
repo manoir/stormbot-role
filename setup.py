10099d7395ae96ad89483bfa914404fe76c0e2a0
Paul Fariello <paul@fariello.eu>
2018-01-25 11:38:04 +0100

    Bump version to 1.4.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='stormbot-role',
-      version='1.3.0',
+      version='1.4.0',
       description='role plugin for stormbot',
       long_description=long_description,
       author='Paul Fariello',
