8ac018661ec39aab3f0e63f5577935d6c5c0e1bd
Paul Fariello <paul@fariello.eu>
2019-05-16 07:14:54 +0200

    Bump version to 1.7.dev2 (peering)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='stormbot-role',
-      version='1.5.0',
+      version='1.7.dev2',
       description='role plugin for stormbot',
       long_description=long_description,
       author='Paul Fariello',
